991b13389b69cf5cdd4b602d7d60b31c885ce2cf
Coder Mish <73038719+codermish@users.noreply.github.com>
2025-05-16 15:34:41 -0400

    Update work3.py

--- work3.py
+++ work3.py
@@ -65,7 +65,11 @@ st.markdown("""
  <div style="background-color: white; padding: 10px 20px; border-radius: 10px; width: fit-content;">
     <img src="https://codermish.github.io/finance/cpe-logo.jpg" width="150">
 </div>
-        <div class="banner-title"  style="margin-top: 10px;">Financial Forecast Dashboard</div>
+        <div class="banner-title"  style="margin-top: 10px;">
+        Financial Forecast Dashboard</div>
+
+        <h1 class="banner-title"  style="margin-top: 10px;">
+        Financial Forecast Dashboard</h1>
 
 </div>
 """, unsafe_allow_html=True)
